91d1e070bd354e642ab49d1ebb8d1c6885433ca3
Matthew Justice <matthewajustice@gmail.com>
2021-01-29 17:09:50 -0500

    Initial commit, react app bootstrapped, main dependencies installed.

---
+++ anywhere-fitness/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>
+        Make sure to <code>"npm install"</code> to download all the dependencies
+      </h1>
+    </>
+  );
+}
+
+export default App;

02bb1501c602b6bc8f87edbb64675b25307a1e3b
Matthew Justice <matthewajustice@gmail.com>
2021-01-30 23:28:22 -0500

    Home page buttons done? still need header buttons and footer

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import { Slide } from '@material-ui/core';
+import { Slide, Fade } from '@material-ui/core';
 
 export default function App() {
   const [slideIn, setSlideIn] = useState({
@@ -9,7 +10,8 @@ export default function App() {
     anytime: false,
     anywhere: false,
   });
-  const timeoutProp = { enter: 700, exit: 0 };
+  const [fadeIn, setFadeIn] = useState(false);
+  const { push } = useHistory();
 
   useEffect(() => {
     setTimeout(() => {
@@ -31,22 +33,40 @@ export default function App() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeIn(true);
+    }, 6500);
+  }, []);
+
   return (
     <Container>
       <TextContent>
         <Slide direction='down' in={slideIn.title} timeout={500}>
           <h1>Health should have no boundaries</h1>
         </Slide>
-        <Slide direction='left' in={slideIn.anytime} timeout={timeoutProp}>
+        <Slide direction='left' in={slideIn.anytime} timeout={700}>
           <h2 className='anytime'>Any time</h2>
         </Slide>
-        <Slide direction='right' in={slideIn.anyone} timeout={timeoutProp}>
+        <Slide direction='right' in={slideIn.anyone} timeout={700}>
           <h2 className='anyone'>Any one</h2>
         </Slide>
-        <Slide direction='up' in={slideIn.anywhere} timeout={timeoutProp}>
+        <Slide direction='up' in={slideIn.anywhere} timeout={700}>
           <h2 className='anywhere'>Any where</h2>
         </Slide>
       </TextContent>
+      <Buttons>
+        <Fade in={fadeIn} timeout={1000}>
+          <div className='button' onClick={() => push('/registration')}>
+            <h3>SIGN UP</h3>
+          </div>
+        </Fade>
+        <Fade in={fadeIn} timeout={1000}>
+          <div className='button' onClick={() => push('/login')}>
+            <h3>LOGIN</h3>
+          </div>
+        </Fade>
+      </Buttons>
     </Container>
   );
 }
@@ -97,5 +117,30 @@ const TextContent = styled.div`
 
   .anywhere {
     background-color: ${(props) => props.theme.midGray};
+  }
+`;
+
+const Buttons = styled.div`
+  margin: 75px;
+  width: 75vw;
+  display: flex;
+  justify-content: space-evenly;
+
+  .button {
+    width: 300px;
+    height: 300px;
+    background-color: ${(props) => props.theme.midGray};
+    color: ${(props) => props.theme.yellow};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: ${(props) => props.theme.bodyFont};
+    font-size: 2rem;
+
+    &:hover {
+      color: whitesmoke;
+      outline: 2px solid ${(props) => props.theme.yellow};
+      cursor: pointer;
+    }
   }
 `;

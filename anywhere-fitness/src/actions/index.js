100839ab7e1ae0bb28ad537db8e3f28f13fc684f
Nathan Hietala <Nrhietala@gmail.com>
2021-01-31 21:23:20 -0600

    Register action/reducer draft

--- anywhere-fitness/src/actions/index.js
+++ anywhere-fitness/src/actions/index.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
+import axios from 'axios'; //eslint-disable-next-line
+import axiosAuth from '../utils/axiosWithAuth';
 
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-export const login = credentials => dispatch => { //credentials will be the state the formValues are contained in
+export const login = credentials => dispatch => { 
   dispatch({ type: LOGIN_START });
     axios
     .post('enter endpoint here that gets token', credentials)
@@ -24,8 +25,21 @@ export const logout = () => dispatch => {
   localStorage.removeItem('token');
 }
 
-export const registration = credentials => dispatch => {
+export const REGISTER_START = "REGISTER_START";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
 
+export const register = credentials => dispatch => {
+  dispatch({ type: REGISTER_START })
+  axios
+  .post('enter endpoint here that gets token', credentials)
+  .then(res => {
+    dispatch({ type:REGISTER_SUCCESS, payload: res.data.token })
+    localStorage.setItem("token", res.data.token)
+  })
+  .catch(err => {
+    dispatch({ type: REGISTER_FAILURE, payload: err.message })
+  })
 }
 
 export const getClasses = id => dispatch => {
@@ -39,17 +53,14 @@ login start ✔
 login success ✔
 login fail ✔
 
-logout start
-logout success
+registration start ✔
+registration success ✔
+registration fail ✔
 
-registration start
-registration success
-registration fail
+logout success ✔
 
 get classes start
 get classes success
 get classes fail
-
-
 
 */

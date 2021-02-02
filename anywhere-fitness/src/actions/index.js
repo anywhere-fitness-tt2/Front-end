b70307f0d4e47da42f568b79962502ed125e9874
Nathan Hietala <Nrhietala@gmail.com>
2021-02-02 10:17:45 -0600

    Login receiving token

--- anywhere-fitness/src/actions/index.js
+++ anywhere-fitness/src/actions/index.js
@@ -6,9 +6,7 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export const login = credentials => dispatch => { 
-  console.log('login action start')
   dispatch({ type: LOGIN_START });
-  console.log('login start')
     axios
     .post('https://af-api-tt2.herokuapp.com/api/auth/login', credentials)
     .then(res => {

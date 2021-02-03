11f0be151210a357fbe7bcd3012e3c4b142eaa48
Nathan Hietala <Nrhietala@gmail.com>
2021-02-02 19:28:03 -0600

    Added actions, instructor/client draft reducers

--- anywhere-fitness/src/reducers/index.js
+++ anywhere-fitness/src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 // import reducers below
 import { loginReducer } from './loginReducer';
 import { registerReducer } from './registerReducer';
+import { instructorReducer } from './instructorReducer';
+import { clientReducer } from './clientReducer';
 
 
 export default combineReducers ({
   // export reducers below
   loginReducer,
   registerReducer,
+  instructorReducer,
+  clientReducer,
 })

7e617d65d1c5512085185b83c61b0836b9800a0d
Matthew Justice <matthewajustice@gmail.com>
2021-02-04 22:39:32 -0500

    client stuff styled to match

--- anywhere-fitness/src/components/SearchCard.js
+++ anywhere-fitness/src/components/SearchCard.js
@@ -2,38 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSearchCard = styled.div`
-display:flex;
-flex-flow:column nowrap;
-align-items:center;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
 
-`
-
-const SearchCard = props => { //eslint-disable-next-line
-  const { signUp, workout : { name, type, time, duration, intensityLvl , location, attendees, maxSize, classId } } = props;
+const SearchCard = (props) => {
+  //eslint-disable-next-line
+  const {
+    signUp,
+    workout: {
+      name,
+      time,
+      duration,
+      intensityLvl,
+      location,
+      attendees,
+      maxSize,
+      classId,
+    },
+  } = props;
 
   const handleEnroll = () => {
-    console.log('signed up!!!!!')
-    signUp(classId)
-  }
+    console.log('signed up!!!!!');
+    signUp(classId);
+  };
 
   return (
-    <StyledSearchCard className="classCard">
-      <h2 className="title">{name}</h2>
-      <h3 className="label">Time</h3>
+    <StyledSearchCard className='classCard'>
+      <h2 className='title'>{name}</h2>
+      <span>Time</span>
       <p>{time}</p>
-      <h3 className="label">Duration</h3>
+      <span>Duration</span>
       <p>{duration}</p>
-      <h3 className="label">Intensity</h3>
+      <span>Intensity</span>
       <p>{intensityLvl}</p>
-      <h3 className="label">Location</h3>
+      <span>Location</span>
       <p>{location}</p>
-      <h3 className="label">Attending</h3>
+      <span>Attending</span>
       <p>{attendees}</p>
-      <h3 className="label">Class Limit</h3>
+      <span>Class Limit</span>
       <p>{maxSize}</p>
       <button onClick={handleEnroll}>Sign Up</button>
     </StyledSearchCard>
-  )
-}
+  );
+};
 
 export default SearchCard;
